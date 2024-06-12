f02be9109bdf0a40abb4526cc6a1b2aec2882498
basvanwesting <basvanwesting@hotmail.com>
2024-06-12 12:15:11 +0200

    Update .config/nvim/lua/autocommands.lua Update .config/nvim/lua/custom/plugins/copilot.lua Update .config/nvim/lua/custom/plugins/copilotchat.lua Update .config/nvim/lua/custom/plugins/neotest.lua Update .config/nvim/lua/custom/plugins/oil.lua Update .config/nvim/lua/custom/plugins/rustaceanvim.lua Update .config/nvim/lua/keymaps.lua Update .config/nvim/lua/kickstart/plugins/autopairs.lua Update .config/nvim/lua/kickstart/plugins/cmp.lua Update .config/nvim/lua/kickstart/plugins/conform.lua Update .config/nvim/lua/kickstart/plugins/debug.lua Update .config/nvim/lua/kickstart/plugins/gitsigns.lua Update .config/nvim/lua/kickstart/plugins/indent_line.lua Update .config/nvim/lua/kickstart/plugins/lint.lua Update .config/nvim/lua/kickstart/plugins/lspconfig.lua Update .config/nvim/lua/kickstart/plugins/mini.lua Update .config/nvim/lua/kickstart/plugins/neo-tree.lua Update .config/nvim/lua/kickstart/plugins/telescope.lua Update .config/nvim/lua/kickstart/plugins/todo-comments.lua Update .config/nvim/lua/kickstart/plugins/tokyonight.lua Update .config/nvim/lua/kickstart/plugins/treesitter.lua Update .config/nvim/lua/kickstart/plugins/which-key.lua Update .config/nvim/lua/lazy-bootstrap.lua Update .config/nvim/lua/lazy-plugins.lua Update .config/nvim/lua/options.lua

--- dot_config/nvim/lua/custom/plugins/rustaceanvim.lua
+++ dot_config/nvim/lua/custom/plugins/rustaceanvim.lua
@@ -1,8 +1,7 @@
 return {
-  {
-    'mrcjkb/rustaceanvim',
-    version = '^4', -- Recommended
-    lazy = false, -- This plugin is already lazy
-  },
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^4", -- Recommended
+		lazy = false, -- This plugin is already lazy
+	},
 }
--- vim: ts=2 sts=2 sw=2 et
