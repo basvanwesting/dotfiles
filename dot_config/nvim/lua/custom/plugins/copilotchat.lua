d811049550163e909300e8510ad07d99083e4574
basvanwesting <basvanwesting@hotmail.com>
2024-06-12 11:57:39 +0200

    Update .config/nvim/lua/custom/plugins/copilotchat.lua Remove .config/nvim/lua/kickstart/plugins/which-key.lua Add .config/nvim/lua/kickstart/plugins/which-key.lua

--- dot_config/nvim/lua/custom/plugins/copilotchat.lua
+++ dot_config/nvim/lua/custom/plugins/copilotchat.lua
@@ -6,7 +6,6 @@ return {
 		dependencies = {
 			{ "zbirenbaum/copilot.lua" },
 			{ "nvim-lua/plenary.nvim" },
-			{ "folke/which-key.nvim" },
 		},
 		keys = {
 			{
@@ -90,9 +89,6 @@ return {
 				end,
 			})
 			require("CopilotChat").setup(opts)
-			require("which-key").register({
-				["<leader>a"] = { name = "[A]I CopilotChat", _ = "which_key_ignore", mode = { "n", "v" } },
-			})
 		end,
 	},
 }
