c75aef820346e5ee9e1ca625a0040fdca9e1fec8
basvanwesting <basvanwesting@hotmail.com>
2024-06-12 18:06:28 +0200

    Update .config/nvim/lua/kickstart/plugins/gitsigns.lua

--- dot_config/nvim/lua/kickstart/plugins/gitsigns.lua
+++ dot_config/nvim/lua/kickstart/plugins/gitsigns.lua
@@ -5,9 +5,9 @@
 -- See `:help gitsigns` to understand what the configuration keys do
 
 -- register top level which-key
-require('which-key').register({
-  ['<leader>h'] = { 'Git [H]unk' },
-}, { mode = 'v' })
+-- require('which-key').register({
+--   ['<leader>h'] = { 'Git [H]unk' },
+-- }, { mode = 'v' })
 
 return {
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
