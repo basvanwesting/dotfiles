00a668cc23e24965724909b81fc0142b81a7c57f
basvanwesting <basvanwesting@hotmail.com>
2024-06-13 13:43:32 +0200

    Update .config/nvim/lua/options.lua

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -15,6 +15,9 @@ vim.opt.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
+-- Don't reload undo history when a file is reloaded
+vim.opt.undofile = false
+
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
