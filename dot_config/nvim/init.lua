14451631621bda3ec8357537d8676425215d2985
basvanwesting <basvanwesting@hotmail.com>
2024-07-09 17:04:03 +0200

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -103,5 +103,8 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
+-- [[ load prerecorded marcros ]]
+vim.cmd('source ' .. vim.fn.stdpath 'config' .. '/vim/macros.vim')
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
