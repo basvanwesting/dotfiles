7fa7fb40be641fc2c8ac28f73fc3763cf30f990d
basvanwesting <basvanwesting@hotmail.com>
2024-10-21 12:21:49 +0200

    Update .config/nvim/lua/custom/plugins/hop.lua

--- dot_config/nvim/lua/custom/plugins/hop.lua
+++ dot_config/nvim/lua/custom/plugins/hop.lua
@@ -6,9 +6,9 @@ return {
     config = function()
       local hop = require 'hop'
       hop.setup()
-      local directions = require('hop.hint').HintDirection
+      -- local directions = require('hop.hint').HintDirection
       vim.keymap.set('', '<leader>f', function()
-        hop.hint_char1 { direction = directions.AFTER_CURSOR }
+        hop.hint_char1()
       end, { desc = 'Hop [f]ind character' })
       -- vim.keymap.set('', '<leader>F', function()
       --   hop.hint_char1 { direction = directions.BEFORE_CURSOR }
