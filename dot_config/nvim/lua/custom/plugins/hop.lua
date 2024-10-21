4d9bd04364c4c53ea9dc04c2c57fe33a4893c978
basvanwesting <basvanwesting@hotmail.com>
2024-10-21 12:16:14 +0200

    Update .config/nvim/lua/custom/plugins/hop.lua

--- dot_config/nvim/lua/custom/plugins/hop.lua
+++ dot_config/nvim/lua/custom/plugins/hop.lua
@@ -2,18 +2,17 @@ return {
   {
     'smoka7/hop.nvim',
     version = '*',
-    opts = {
-      keys = 'etovxqpdygfblzhckisuran',
-    },
+    lazy = false,
     config = function()
       local hop = require 'hop'
+      hop.setup()
       local directions = require('hop.hint').HintDirection
       vim.keymap.set('', '<leader>f', function()
         hop.hint_char1 { direction = directions.AFTER_CURSOR }
-      end, {})
-      vim.keymap.set('', '<leader>F', function()
-        hop.hint_char1 { direction = directions.BEFORE_CURSOR }
-      end, {})
+      end, { desc = 'Hop [f]ind character' })
+      -- vim.keymap.set('', '<leader>F', function()
+      --   hop.hint_char1 { direction = directions.BEFORE_CURSOR }
+      -- end, {})
     end,
   },
 }
