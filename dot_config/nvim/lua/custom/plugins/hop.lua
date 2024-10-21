84be8b84156ebad800085ce20ca7aa7e614156e2
basvanwesting <basvanwesting@hotmail.com>
2024-10-21 12:29:57 +0200

    Update .config/nvim/lua/custom/plugins/hop.lua

--- dot_config/nvim/lua/custom/plugins/hop.lua
+++ dot_config/nvim/lua/custom/plugins/hop.lua
@@ -5,11 +5,14 @@ return {
     lazy = false,
     config = function()
       local hop = require 'hop'
-      hop.setup()
-      -- local directions = require('hop.hint').HintDirection
+      hop.setup {
+        multi_windows = true,
+      }
       vim.keymap.set('', '<leader>f', function()
-        hop.hint_char1()
-      end, { desc = 'Hop [f]ind character' })
+        hop.hint_char2()
+      end, { desc = 'Hop [f]ind characters' })
+
+      -- local directions = require('hop.hint').HintDirection
       -- vim.keymap.set('', '<leader>F', function()
       --   hop.hint_char1 { direction = directions.BEFORE_CURSOR }
       -- end, {})
