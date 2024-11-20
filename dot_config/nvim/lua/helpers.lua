1217a91511185d0729241f69aae8934479dd00c9
basvanwesting <basvanwesting@hotmail.com>
2024-11-20 13:41:49 +0100

    Update .config/nvim/lua/helpers.lua

--- dot_config/nvim/lua/helpers.lua
+++ dot_config/nvim/lua/helpers.lua
@@ -1,11 +1,7 @@
 local helpers = {}
 
-function helpers.get_current_buffer_absolute_dir()
-  return require('oil').get_current_dir() or vim.fn.expand '%:p'
-end
-
 function helpers.get_current_buffer_relative_dir()
-  return vim.fn.fnamemodify(helpers.get_current_buffer_absolute_dir())
+  return vim.fn.fnamemodify(require('oil').get_current_dir() or vim.fn.expand '%:h', ':~:.')
 end
 
 return helpers
