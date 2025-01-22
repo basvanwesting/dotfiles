8c1dc4da99c4dc201b22c76769521e4628a41f18
basvanwesting <basvanwesting@hotmail.com>
2025-01-22 15:56:51 +0100

    Update .config/nvim/lua/helpers.lua Update .config/nvim/lua/lazy-plugins.lua

--- dot_config/nvim/lua/helpers.lua
+++ dot_config/nvim/lua/helpers.lua
@@ -1,8 +1,8 @@
 local helpers = {}
 
 function helpers.get_current_buffer_relative_dir()
-  -- return vim.fn.fnamemodify(require('oil').get_current_dir() or vim.fn.expand '%:h', ':~:.')
-  return vim.fn.expand '%:h', ':~:.'
+  -- return vim.fn.expand '%:h', ':~:.'
+  return vim.fn.fnamemodify(require('oil').get_current_dir() or vim.fn.expand '%:h', ':~:.')
 end
 
 return helpers
