eec194f598b23f168280969e51536af81f81ac60
basvanwesting <basvanwesting@hotmail.com>
2025-02-12 15:54:03 +0100

    Update .config/nvim/lua/keymaps.lua Update .config/nvim/lua/kickstart/plugins/gitsigns.lua Update .config/nvim/lua/kickstart/plugins/todo-comments.lua

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -6,14 +6,14 @@ vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [d]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [d]iagnostic message' })
+-- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [d]iagnostic message' })
+-- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [d]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [e]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [q]uickfix list' })
 
 -- Quickfix keymaps
-vim.keymap.set('n', '[q', ':cprev<CR>')
-vim.keymap.set('n', ']q', ':cnext<CR>')
+-- vim.keymap.set('n', '[q', ':cprev<CR>')
+-- vim.keymap.set('n', ']q', ':cnext<CR>')
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
