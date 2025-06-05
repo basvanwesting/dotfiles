2be51054b5e5d4c5b58e83deffa51d2eda3d2dc0
basvanwesting <basvanwesting@hotmail.com>
2025-06-05 17:18:36 +0200

    Revert "add stylua shimmed command"
    This reverts commit 7c7a30eaf022279d271271cb451595b98170b7d4.

--- dot_config/nvim/lua/kickstart/plugins/conform.lua
+++ dot_config/nvim/lua/kickstart/plugins/conform.lua
@@ -25,8 +25,7 @@ return {
         }
       end,
       formatters_by_ft = {
-        -- lua = { 'stylua' },
-        lua = { vim.fs.normalize '~/.asdf/shims/stylua' },
+        lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
